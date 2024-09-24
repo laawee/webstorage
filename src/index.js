4a68455409534b6c47bddece6ef243c834e4dce0
clickTeam <43138398+laawee@users.noreply.github.com>
2024-09-24 10:42:54 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './App';  // 确保这里的 'App' 与文件名大小写完全匹配
 
 ReactDOM.render(
   <React.StrictMode>
