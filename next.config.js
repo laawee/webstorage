737094ee2eaf6225ef37d9ec89365bcdc468ea62
clickTeam <43138398+laawee@users.noreply.github.com>
2024-09-26 11:04:43 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,24 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+{
+  "name": "quark-cloud-storage",
+  "version": "0.1.0",
+  "private": true,
+  "scripts": {
+    "dev": "next dev",
+    "build": "next build",
+    "start": "next start",
+    "lint": "next lint"
+  },
+  "dependencies": {
+    "@aws-sdk/client-s3": "^3.32.0",
+    "next": "latest",
+    "react": "latest",
+    "react-dom": "latest",
+    "antd": "^4.16.13",
+    "@ant-design/icons": "^4.7.0"
+  },
+  "devDependencies": {
+    "@types/react": "latest",
+    "@types/node": "latest",
+    "typescript": "latest"
+  }
 }
-
-module.exports = nextConfig
