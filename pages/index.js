021cd9d8acf33fb942554f01157df3092bd23fa2
clickTeam <43138398+laawee@users.noreply.github.com>
2024-09-26 10:39:08 +0800

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import QuarkCloudStorage from '../components/QuarkCloudStorage'
+
+export default function Home() {
+  return (
+    <div className="App">
+      <QuarkCloudStorage />
+    </div>
+  )
+}
